d8962c989b5a747607a762e6c01be76f198fcc17
Kevin Hink <kevin.hink99mail.com>
2019-10-15 14:26:28 +0200

    Hauptseite erzeugt

--- app/src/main/java/com/example/grenrechner/MainActivity.java
+++ app/src/main/java/com/example/grenrechner/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.grenrechner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titel;
+    TextView beschreibung;
+    Button berechnen;
+    Button hilfe;
+    EditText groesseMutter;
+    EditText groesseVater;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titel=findViewById(R.id.Titel);
+        beschreibung=findViewById(R.id.Beschreibung);
+        groesseMutter=findViewById(R.id.groesseMutter);
+        groesseVater=findViewById(R.id.groesseVater);
+        berechnen=findViewById(R.id.ButtonBerechnen);
+        hilfe=findViewById(R.id.ButtonHilfe);
     }
 }
